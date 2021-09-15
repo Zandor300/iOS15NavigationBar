a1e76fb224b53b3b33546de8a65333040a408e45
Zandor Smith <info@zsinfo.nl>
2021-09-15 10:53:40 +0200

    Initial Commit

---
+++ iOS15NavigationBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS15NavigationBar
+//
+//  Created by Zandor Smith on 15/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
